dce07125ca7d3a647c821edb4fcad2db8bbdd2b3
IsaquePorto <isaqueporto2016@gmail.com>
2022-05-30 21:41:12 -0300

    Criacao do sidenav e da navegação entre as telas

---
+++ src/app/tela-despesas/tela-despesas.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TelaDespesasComponent } from './tela-despesas.component';
+
+describe('TelaDespesasComponent', () => {
+  let component: TelaDespesasComponent;
+  let fixture: ComponentFixture<TelaDespesasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ TelaDespesasComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TelaDespesasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
